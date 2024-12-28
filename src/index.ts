f79832c2f092268c0296766248c0dd3916bea7bd
Bartosz Kut <bartosz.kut@schibsted.com>
2024-12-28 17:32:13 +0100

    Solution for the task: s02e01

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { Elysia } from 'elysia';
 import { verify } from './s01e02-verify';
 import { fix } from './s01e03-fix';
 import { censorship } from './s01e05-report';
+import { mp3 } from './s02e01-mp3';
 
 const app = new Elysia()
     .get('/', () => 'Hello Elysia')
     .get('/verify', () => verify())
     .get('/fix', () => fix())
     .get('/censorship', () => censorship())
+    .get('/mp3', () => mp3())
     .listen(3000);
 
 console.log(

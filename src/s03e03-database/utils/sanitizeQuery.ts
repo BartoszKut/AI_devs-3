91de477cb8d80d15c58a3782df04297b03ef0757
Bartosz Kut <bartosz.kut@schibsted.com>
2025-01-12 15:21:24 +0100

    Solutin for the task: S03E03

---
+++ src/s03e03-database/utils/sanitizeQuery.ts
@@ -0,0 +1,9 @@
+export const sanitizeQuery = (sqlQuery: string) => {
+    const trimmedQuery = sqlQuery.replace(/`/g, '').replace(/sql/g, '').trim();
+
+    if (!trimmedQuery.toUpperCase().startsWith('SELECT')) {
+        throw new Error('Wrong query: Expected SELECT query.');
+    }
+
+    return trimmedQuery;
+}
